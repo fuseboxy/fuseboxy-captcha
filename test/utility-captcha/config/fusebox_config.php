fbe00f66b210104554e1bfb9a594c863da365a69
Henry Shum <henry@metaseit.com>
2017-04-26 12:11:31 +0800

    prepare utility for captcha unit test

---
+++ test/utility-captcha/config/fusebox_config.php
@@ -0,0 +1,6 @@
+<?php
+return array('reCAPTCHA' => array(
+	'sitekey' => '6LfSMhoUAAAAAAT2CHRj-kmMkOPFHxrF3lVfLuHR',
+	'secret'  => '6LfSMhoUAAAAACF-DsDJ-k-CDLhEiAgC9GpYIxE4',
+	'verify'  => 'https://www.google.com/recaptcha/api/siteverify',
+));
